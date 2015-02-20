b646db9899dd7e7fc8ee1b8f623988bfb43a477f
“oscarmartinezrubi” <omrubi@gmail.com>
2015-02-20 11:40:08 +0100

    Add abstract loader and abstract querier

---
+++ AbstractLoader.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+################################################################################
+#    Created by Oscar Martinez                                                 #
+#    o.rubi@esciencecenter.nl                                                  #
+################################################################################
+import os, time, multiprocessing, logging, sys
+from pointcloud import utils
+
+class AbstractLoader:
+    """Abstract class for the loaders to be implemented for each different 
+    solution for the benchmark"""
+    def __init__(self, configuration):
+        """ Create the loader, a ConfigParser is required (loaded from a .ini file)"""
+        self.conf = configuration
+    
+    def getConfiguration(self):
+        """ Gets the configuration (ConfigParser) instance """
+        return self.conf
+    
+    def run(self):
+        """ Executes the load method while monitoring performance """
+        config = self.getConfiguration()
+        # Create and move to the execution path
+        executionPath = config.get('General','ExecutionPath')
+        if not os.path.isdir(executionPath):
+            os.makedirs(executionPath)
+        os.chdir(executionPath)
+        usageMonitor = config.get('General','UsageMonitor').lower()
+        ioMonitorParam = config.get('General','IOMonitor').strip()
+        ioMonitor = False
+        ioDevices = None
+        if ioMonitorParam != '':
+            ioMonitor = True
+            ioDevices = ioMonitorParam.split(',')
+        usageMethod = None
+        if usageMonitor == 'python':
+            usageMethod = utils.getUsagePy
+        elif usageMonitor == 'top':
+            usageMethod = utils.getUsageTop
+        elif usageMonitor == 'ps':
+            usageMethod = utils.getUsagePS
+        elif usageMonitor != '':
+            raise Exception('ERROR: UsageMonitor must be python, ps or top')
+        
+        
+        resultsFileAbsPath = os.path.abspath('loading_' + config.get('Query','OutputFileName'))
+        usageAbsPath = os.path.abspath('loading.usage')
+        usageImageAbsPath = os.path.abspath('loading_usage.png')
+        ioAbsPath = None
+        if ioMonitor:
+            ioAbsPath = os.path.abspath('loading.io')
+            ioImageAbsPath = os.path.abspath('loading_io.png')
+        
+        t0 = time.time()
+        utils.runMonitor(self.load, (resultsFileAbsPath,), usageMethod, usageAbsPath, ioDevices, ioAbsPath)
+        totalTime = time.time()-t0
+        cpu = '-'
+        mem = '-'
+        if usageMethod != None:
+            (times, cpus, mems) = utils.parseUsage(usageAbsPath)
+            utils.saveUsage(times, cpus, mems, 'Loading CPU/MEM', usageImageAbsPath)
+            cpu = '%.2f' % cpus.mean()
+            mem = '%.2f' % mems.mean()
+        if ioMonitor:
+            (times, rdata, wdata) = utils.parseIO(ioAbsPath)
+            utils.saveIO(times, rdata, wdata, 'Loading IO', ioImageAbsPath)
+        resultsFile = open(resultsFileAbsPath,'a')
+        size = self.size()
+        numpoints = 'Total Num. points: ' + str(self.getNumPoints())
+        logging.info('Total time is: %.2f seconds' % totalTime)
+        resultsFile.write('total: %.2f \n'%totalTime)
+        logging.info(size)
+        resultsFile.write(str(size) + '\n')
+        logging.info(numpoints)
+        resultsFile.write(str(numpoints) + '\n')
+        logging.info('Avg. CPU: ' + cpu + '. Avg. Memory: ' + mem)
+        resultsFile.write('Avg. CPU: '+ cpu + '. Avg. Memory: \n' + mem)
+        resultsFile.close()
+        logging.info('See ' + resultsFileAbsPath)
+        logging.info('See ' + usageAbsPath)
+        logging.info('See ' + usageImageAbsPath)
+        if ioMonitor:
+            logging.info('See ' + ioAbsPath)
+            logging.info('See ' + ioImageAbsPath)
+
+        
+    def load(self, resultsFileName):
+        """ Loads the data """
+        resultsFile = open(resultsFileName,'w')
+        # Run the initialization
+        ti = time.time()
+        self.initialize()
+        t = time.time() - ti
+        logging.info('initialize finished in ' + ('%.2f' % t) + ' seconds')
+        resultsFile.write('  initialize: %.2f \n'%t)
+        
+        # Runs process which will process al input data
+        results = self.process()
+        for (inputItem, t) in results:
+            resultsFile.write('    ' + inputItem + (': %.2f \n'%t))
+        
+        # Run close
+        ti = time.time()
+        self.close()
+        t = time.time() - ti
+        logging.info('close finished in ' + ('%.2f' % t) + ' seconds')
+        resultsFile.write('  close: %.2f \n'%t)
+        resultsFile.close()
+        
+    def logProcessItemInfo(self, inputItem, elapsedTime, itemCounter, numItems):
+        logging.info('load from ' + inputItem + ' finished in ' + ('%.2f' % elapsedTime) + ' seconds. ' + str(itemCounter+1) + '/' + str(numItems) + ' (' + ('%.2f' % (float(itemCounter+1) * 100. / float(numItems))) + '%)')
+    
+    def processSingle(self, inputItems, processItem):
+        """ Process the input items with a single core/process"""
+        numItems = len(inputItems)
+        if numItems == 0:
+            raise Exception('No files/folders found with inputFolder/Extension')
+
+        results = []
+        for i in range(numItems):
+            inputItem = inputItems[i]
+            ti = time.time()
+            processItem(i, inputItem)
+            t = time.time() - ti
+            self.logProcessItemInfo(inputItem, t, i, numItems)
+            results.append([inputItem, t])
+        return results
+    
+    def processMulti(self, inputItems, numProcessesLoad, processItemParallelMethod, processItemSequentialMethod = None, assertOrderSequentialMethod = False):
+        """ Process the input items with a multiple cores/processes. 
+The method that is parallelized is given by processItemParallelMethod.
+If provided processItemSequentialMethod, this method is executed not-parallely after each parallel process is finished (while parent process is waiting for the children processes)
+If assertOrderSequentialMethod = True we assert that the processItemSequentialMethod is executed in same order as input items
+"""
+        numItems = len(inputItems)
+        if numItems == 0:
+            raise Exception('No files/folders found with inputFolder/Extension') 
+        # We create a tasts queue and fill it with as many tasks as input files
+        childrenQueue = multiprocessing.Queue()
+        for i in range(numItems):
+            inputItem = inputItems[i]
+            childrenQueue.put([i, inputItem])
+        for i in range(numProcessesLoad): #we add as many None jobs as self.numProcessesLoad to tell them to terminate (queue is FIFO)
+            childrenQueue.put(None)
+            
+        resultQueue = multiprocessing.Queue() # The queue where to put the results
+        children = []
+        # We start numProcessesLoad children processes
+        for i in range(numProcessesLoad):
+            children.append(multiprocessing.Process(target=self.runChildLoad,
+                args=(i, childrenQueue, resultQueue, processItemParallelMethod)))
+            children[-1].start()
+        
+        # Collect the results
+        results = []
+        if assertOrderSequentialMethod:
+            nextFile = 0
+            finishedItemsIds = {}
+            fileCounter = 0
+            while fileCounter < numItems or nextFile < numItems:
+                if nextFile in finishedItemsIds:
+                    (inputItem, t) = finishedItemsIds.pop(nextFile)
+                    if processItemSequentialMethod != None:
+                        processItemSequentialMethod(inputItem, nextFile, numItems)
+                    self.logProcessItemInfo(inputItem, t, nextFile, numItems)
+                    results.append([inputItem, t])
+                    nextFile += 1
+                else:
+                    [identifier, inputItem, t] = resultQueue.get()
+                    finishedItemsIds[identifier] = (inputItem, t)
+                    fileCounter += 1
+        else:            
+            for i in range(numItems):        
+                [identifier, inputItem, t] = resultQueue.get()
+                if processItemSequentialMethod != None:
+                    processItemSequentialMethod(inputItem, identifier, numItems)
+                self.logProcessItemInfo(inputItem, t, i, numItems)
+                results.append([inputItem, t])
+        
+        # wait for all children to finish their execution
+        for i in range(numProcessesLoad):
+            children[i].join()
+            
+        return results
+    
+    def runChildLoad(self, childIndex, childrenQueue, resultQueue, processItemParallelMethod):
+        kill_received = False
+        while not kill_received:
+            job = None
+            try:
+                # This call will patiently wait until new job is available
+                job = childrenQueue.get()
+            except:
+                # if there is an error we will quit the loop
+                kill_received = True
+            if job == None:
+                # If we receive a None job, it means we can stop the grandchildren too
+                kill_received = True
+            else:            
+                [identifier, inputItem] = job
+                ti = time.time()
+                try:
+                    processItemParallelMethod(identifier, inputItem)
+                except Exception,e:
+                    logging.exception('ERROR loading from ' + inputItem + ':\n' + str(e))
+                resultQueue.put([identifier, inputItem, time.time() - ti])
+    #
+    # FOLLOWING METHODS HAVE TO BE IMPLEMENTED BY ALL LOADERS
+    # 
+    def process(self):
+        """ Process the input data """
+        raise NotImplementedError( "Should have implemented this" )
+        
+    def connect(self, super = False):
+        """ Gets a new connection to the DB """
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def initialize(self):
+        """ Initialize the DB, load possible extensions and create tables """
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def close(self):
+        """ Close the loader """
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def size(self):
+        """ Get the DB size """
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def getNumPoints(self):
+        """ Get the total number of loaded points """
+        return 0
+        #raise NotImplementedError( "Should have implemented this" )
+    
---
+++ AbstractQuerier.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+################################################################################
+#    Created by Oscar Martinez                                                 #
+#    o.rubi@esciencecenter.nl                                                  #
+################################################################################
+import os, multiprocessing
+from pointcloud.QueriesParameters import QueriesParameters
+from pointcloud import utils
+
+class AbstractQuerier:
+    """Abstract class for the queriers to be implemented for each different 
+    solution for the benchmark."""
+    def __init__(self, configuration):
+        """ Create the querier, a ConfigParser is required (loaded from a .ini file)"""
+        self.conf = configuration
+         
+    def getConfiguration(self):
+        """ Gets the configuration (ConfigParser) instance """
+        return self.conf
+    
+    def connect(self, superUser = False):
+        """ Gets a new connection to the DB """
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def initialize(self):
+        """ Initialize the querier """
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def run(self):
+        config = self.getConfiguration()
+        
+        # Create and move to the execution path
+        executionPath = config.get('General','ExecutionPath')
+        if not os.path.isdir(executionPath):
+            os.makedirs(executionPath)
+        os.chdir(executionPath)    
+
+        # Initialize the PCDMS for querying
+        self.initialize()        
+        
+        # Get which IO devices need to be monitored 
+        ioMonitorParam = config.get('General','IOMonitor').strip()
+        ioDevices = None
+        if ioMonitorParam != '':
+            ioDevices = ioMonitorParam.split(',')
+        # Get the method to monitor the system usage (CPU / memory)
+        # Nothe that all system is monitored (not only the processes related to the queries)
+        usageMonitor = config.get('General','UsageMonitor').lower()
+        usageMethod = None
+        if usageMonitor == 'python':
+            usageMethod = utils.getUsagePy
+        elif usageMonitor == 'top':
+            usageMethod = utils.getUsageTop
+        elif usageMonitor == 'ps':
+            usageMethod = utils.getUsagePS
+        elif usageMonitor != '':
+            raise Exception('ERROR: UsageMonitor must be python, ps or top')
+        
+        #manager = multiprocessing.Manager()
+        #returnDict = manager.dict()
+        
+        
+        # Read the query file 
+        queryFileAbsPath = config.get('Query','File')
+        queriesParameters = QueriesParameters(queryFileAbsPath)
+        
+        # Get the identifiers of the several queries
+        queryIds = queriesParameters.getIds()
+        numQueries = len(queryIds) 
+        if numQueries != len(set(queryIds)):
+            raise Exception('ERROR: There are duplicated identifiers in given XML')
+        
+        numUsers = config.getint('Query','NumberUsers')
+        numIterations = config.getint('Query','NumberIterations')
+        
+        multiUser = (numUsers>1) or config.getboolean('Query','MultiUser')
+
+        # Create queues
+        queriesQueue = multiprocessing.Queue() # The queue of tasks (queries)
+        resultsQueue = multiprocessing.Queue() # The queue of results
+        
+        for queryId in queryIds:
+            queriesQueue.put(queryId)
+        for i in range(numUsers): #we add as many None jobs as numUsers to tell them to terminate (queue is FIFO)
+            queriesQueue.put(None)
+        
+        users = []
+        # We start numUsers users processes
+        for i in range(numUsers):
+            users.append(multiprocessing.Process(target=self.runChild, 
+                args=(i, queriesQueue, resultsQueue, multiUser, numIterations, queriesParameters, usageMethod, ioDevices)))
+            users[-1].start()
+        
+        # We need to receive for each query the two iterations and for each iteration both the results from the query execution and from the monitor 
+        numResults = numQueries * numIterations
+        
+        resultsDict = {}
+        for i in range(numResults):
+            [userIndex, queryId, iterationId, qTime, qResult, qCPU, qMEM] = resultsQueue.get()
+            resultsDict[(queryId, iterationId)] = (userIndex, qTime, qResult, qCPU, qMEM)
+        # wait for all users to finish their execution
+        for i in range(numUsers):
+            users[i].join()
+        
+        stats = []
+        for queryId in queryIds:
+            for iterationId in range(numIterations):
+                (userIndex, qTime, qResult, qCPU, qMEM) = resultsDict[(queryId, iterationId)]
+                qName = str(queryId) + '_' + str(iterationId)
+                stats.append((qName, qTime, qResult, qCPU, qMEM))
+
+        self.close()
+        return stats
+    
+    def runChild(self, userIndex, tasksQueue, resultsQueue, multiUser, numIterations, queriesParameters, usageMethod, ioDevices):
+        childResultQueue = multiprocessing.Queue()
+        kill_received = False
+        while not kill_received:
+            queryId = None
+            try:
+                # This call will patiently wait until new job is available
+                queryId = tasksQueue.get()
+            except:
+                # if there is an error we will quit the generation
+                kill_received = True
+            if queryId == None:
+                # If we receive a None job, it means we can stop this workers 
+                # (all the create-image jobs are done)
+                kill_received = True
+            else:            
+                for iterationId in range(numIterations):
+                    queryName = queryId + '_' + '_' + str(iterationId)
+                    usageAbsPath = os.path.abspath(queryName + '.usage')
+                    
+                    ioAbsPath = None
+                    if ioDevices != None:
+                        ioAbsPath = os.path.abspath(queryName + '.io')
+                        
+                    utils.runMonitor(self.runQuery,(queryId, iterationId, queriesParameters, childResultQueue, multiUser), usageMethod, usageAbsPath, ioDevices, ioAbsPath)
+                    
+                    [queryId, iterationId, qTime, qResult] = childResultQueue.get()
+                    
+                    (times, cpus, mems) = utils.parseUsage(usageAbsPath)
+                    (qCPU,qMEM) = (cpus.mean(), mems.mean())
+                    imageAbsPath = os.path.abspath(queryName + '_usage.png')
+                    utils.saveUsage(times, cpus, mems, queryName + ' CPU/MEM', imageAbsPath)
+                    if ioDevices != None:
+
+
+                        (times, rdata, wdata) = utils.parseIO(ioAbsPath)
+                        ioImageAbsPath = os.path.abspath(queryName + '_io.png')
+                        utils.saveIO(times, rdata, wdata, queryName + ' IO', ioImageAbsPath)
+
+                    resultsQueue.put((userIndex, queryId, iterationId, qTime, qResult, qCPU, qMEM))   
+                    
+    def runQuery(self, queryId, iterationId, queriesParameters, resultsQueue, multiUser):
+        try:
+            if not multiUser:
+                (eTime, result) = self.query(queryId, iterationId, queriesParameters)
+            else:
+                (eTime, result) = self.queryMulti(queryId, iterationId, queriesParameters)
+            resultsQueue.put((queryId, iterationId, eTime, result))
+        except Exception,e:
+            print e
+            resultsQueue.put((queryId, iterationId, '-', '-'))
+    
+    def query(self, queryId, iterationId, queriesParameters):
+        """ Executes query indicated by queryId. It must return a tuple with (time, results)"""
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def queryMulti(self, queryId, iterationId, queriesParameters):
+        """ Executes query indicated by queryId. It must return a tuple with (time, results)"""
+        raise NotImplementedError( "Should have implemented this" )
+    
+    def close(self):
+        """ Close and connection with DB """
+        raise NotImplementedError( "Should have implemented this" )
