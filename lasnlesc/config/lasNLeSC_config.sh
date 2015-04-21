98a1a82b0c69de9cb56e2e7378864495210ae863
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2015-04-21 13:44:18 +0200

    Add missing directory as cmake complains about...

--- lasnlesc/config/lasNLeSC_config.sh
+++ lasnlesc/config/lasNLeSC_config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export LASNLESC_HOME=/home/oscar/sw/lasnlesc
-export LIBLAS_HOME=/home/oscar/sw/liblas/build
-export LASZIP_HOME=/home/oscar/sw/laszip-2.1.0/build
-export BOOST_HOME=/usr/local
-export BOOST_ROOT=/usr/local
+export LASNLESC_HOME=`pwd`
+export LIBLAS_HOME=$LIBLAS17_HOME
+export LASZIP_HOME=$LASZIP21_HOME
+export BOOST_HOME=$BOOST155_HOME
+export BOOST_ROOT=$BOOST_HOME
 
 export CMAKE_MODULE_PATH=$LIBLAS_HOME:${CMAKE_MODULE_PATH}
 export LD_RUN_PATH=$BOOST_HOME/lib:$LASZIP_HOME/lib:$LIBLAS_HOME/lib:$LASNLESC_HOME/lib:$LD_RUN_PATH
@@ -11,4 +11,4 @@ export LD_LIBRARY_PATH=$BOOST_HOME/lib:$LASZIP_HOME/lib:$LIBLAS_HOME/lib:$LASNLE
 export LIBRARY_PATH=$BOOST_HOME/lib:$LASZIP_HOME/lib:$LIBLAS_HOME/lib:$LASNLESC_HOME/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=$LIBLAS_HOME/include/liblas/capi:${C_INCLUDE_PATH}
 
-export LDFLAGS="${LDFLAGS} -L$BOOST_HOME/lib -L$LIBLAS_HOME/lib -L$LASZIP_HOME/lib -llas_c -llaszip"
+export LDFLAGS="${LDFLAGS} -L$BOOST_HOME/lib -L$LIBLAS_HOME/lib -L$LASZIP_HOME/lib -llas_c -llaszip -lpthread"
