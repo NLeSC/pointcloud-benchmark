69aebdb7b65ce59b9670aea2bae50ceb45cd3301
“oscar.martinez.rubi” <omrubi@gmail.com>
2015-04-22 12:07:20 +0200

    test new create grid table with non overlapping tiles

--- python/pointcloud/oracle/AbstractQuerier.py
+++ python/pointcloud/oracle/AbstractQuerier.py
@@ -35,21 +35,70 @@ class AbstractQuerier(AQuerier, CommonOracle):
     def addContainsCondition(self, queryParameters, queryArgs, xname, yname):
         return (None, "(select geom from " + self.queryTable + " where id = " + str(self.queryIndex) + "), " + str(self.tolerance) + ", NULL ")    
     
+#     def createGridTableMethod(self, cursor, gridTable, ncols, nrows):
+#         query = """
+# CREATE TABLE """ + gridTable + """ AS 
+# WITH A AS (
+#     SELECT geom FROM """ + self.queryTable + """ where id = """ + str(self.queryIndex) + """
+# )
+# SELECT T.id, SDO_GEOM.SDO_INTERSECTION(A.geom, T.GEOMETRY, """ + str(self.tolerance) + """) as geom FROM 
+#     TABLE(select sdo_sam.tiled_bins(
+#         SDO_GEOM.SDO_MIN_MBR_ORDINATE(a.geom,1),
+#         SDO_GEOM.SDO_MAX_MBR_ORDINATE(a.geom,1),
+#         SDO_GEOM.SDO_MIN_MBR_ORDINATE(a.geom,2),
+#         SDO_GEOM.SDO_MAX_MBR_ORDINATE(a.geom,2),
+#         null, """ + str(self.srid) + """, """ + str(ncols) +""" - 1, """ + str(nrows) +""" - 1
+#     ) from A) T, A
+# """
+#         oracleops.mogrifyExecute(cursor, query)
+#         cursor.execute("CREATE INDEX " + gridTable + "_id_idx ON " + gridTable + "(ID)")
+#         cursor.connection.commit()
+
     def createGridTableMethod(self, cursor, gridTable, ncols, nrows):
+        (minX, maxX) = (self.qp.minx, self.qp.maxx)
+        (minY, maxY) = (self.qp.miny, self.qp.maxy)
+        rangeX = maxX - minX 
+        rangeY = maxY - minY
+        
+        tileSizeX = rangeX / float(ncols)
+        tileSizeY = rangeY / float(nrows)
+        
+        scaleX = 0.01
+        scaleY = 0.01
+        
+        tilesTableName = "TEMP_" + gridTable
+        tileCounter = 0
+        
+        cursor.execute("""
+    CREATE TABLE """ + tilesTableName + """ (
+        id integer,
+        geom public.geometry(Geometry,""" + str(self.srid) + """)
+    )""")
+        
+        for xIndex in range(ncols):
+            for yIndex in range(nrows):
+                minTileX = minX + (xIndex * tileSizeX)
+                maxTileX = minX + ((xIndex+1) * tileSizeX)
+                minTileY = minY + (yIndex * tileSizeY)
+                maxTileY = minY + ((yIndex+1) * tileSizeY)
+                # To avoid overlapping tiles
+                if xIndex < axisTiles-1:
+                    maxTileX -= scaleX
+                if yIndex < axisTiles-1:
+                    maxTileY -= scaleY
+                    
+                #print '\t'.join((str(xIndex), str(yIndex), '%.2f' % minTileX, '%.2f' % minTileY, '%.2f' % maxTileX, '%.2f' % maxTileY))
+                insertStatement = "INSERT INTO " + tilesTableName + """(id,geom) VALUES (%s, ST_MakeEnvelope(%s, %s, %s, %s, %s));"""
+                insertArgs = [tileCounter, minTileX, minTileY, maxTileX, maxTileY, self.srid]
+                cursor.execute(insertStatement, insertArgs)
+                
+                tileCounter += 1
+        
         query = """
-CREATE TABLE """ + gridTable + """ AS 
-WITH A AS (
-    SELECT geom FROM """ + self.queryTable + """ where id = """ + str(self.queryIndex) + """
-)
-SELECT T.id, SDO_GEOM.SDO_INTERSECTION(A.geom, T.GEOMETRY, """ + str(self.tolerance) + """) as geom FROM 
-    TABLE(select sdo_sam.tiled_bins(
-        SDO_GEOM.SDO_MIN_MBR_ORDINATE(a.geom,1),
-        SDO_GEOM.SDO_MAX_MBR_ORDINATE(a.geom,1),
-        SDO_GEOM.SDO_MIN_MBR_ORDINATE(a.geom,2),
-        SDO_GEOM.SDO_MAX_MBR_ORDINATE(a.geom,2),
-        null, """ + str(self.srid) + """, """ + str(ncols) +""" - 1, """ + str(nrows) +""" - 1
-    ) from A) T, A
-"""
+SELECT T.id, SDO_GEOM.SDO_INTERSECTION(A.geom, T.geom, """ + str(self.tolerance) + """) as geom 
+FROM  """ + self.queryTable + """ A, """ + tilesTableName + """ T
+WHERE A.id  = """ + str(self.queryIndex)
+        
         oracleops.mogrifyExecute(cursor, query)
         cursor.execute("CREATE INDEX " + gridTable + "_id_idx ON " + gridTable + "(ID)")
         cursor.connection.commit()
