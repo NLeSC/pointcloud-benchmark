bcd5c182413795819214cbe3670fb115158cf55e
“oscar.martinez.rubi” <omrubi@gmail.com>
2015-04-22 17:34:04 +0200

    Finished generate_tiles

--- python/pcviewer/generate_tiles.py
+++ python/pcviewer/generate_tiles.py
@@ -1,5 +1,12 @@
-import argparse, traceback, time, os, math, psycopg2
-#from pointcloud import lasops, postgresops
+import argparse, traceback, time, os, math
+#from pointcloud import lasops
+
+import shapely
+from shapely.wkt import loads
+from shapely.geometry import box
+from de9im.patterns import intersects, contains
+
+ONLY_SHOW = False
 
 def argument_parser():
     """ Define the arguments and return the parser object"""
@@ -9,97 +16,117 @@ def argument_parser():
     parser.add_argument('-s','--srid',default='',help='SRID of the input data',type=int, required=True)
     parser.add_argument('-o','--output',default='',help='Output data folder for the different tiles',type=str, required=True)
     parser.add_argument('-n','--number',default='',help='Number of tiles',type=int, required=True)
-    parser.add_argument('-d','--dbname',default='',help='PostgreSQL DB name',type=str, required=True)
-    parser.add_argument('-u','--dbuser',default='',help='DB user',type=str, required=True)
-    parser.add_argument('-p','--dbpass',default='',help='DB pass',type=str, required=True)
-    parser.add_argument('-b','--dbhost',default='',help='DB host',type=str, required=True)
-    parser.add_argument('-r','--dbport',default=5432,help='DB port [default 5432]',type=int)
     return parser
 
-def run(inputFolder, srid, outputFolder, numberTiles, dbName, dbUser, dbPass, dbHost, dbPort):
+def _relation(geom1, geom2):
+    """ Returns the relationship between two geometries. 
+          0 if they are disjoint, 
+          1 if geom2 is completely in geom1,
+          2 if geom2 is partly in geom1"""
+    relation = geom1.relate(geom2)
+    if not intersects.matches(relation):
+        return 0 # they are disjoint
+    elif contains.matches(relation):
+        return 1 
+    else: # there is some overlaps
+        return 2
+
+def executeCommand(command):
+    print command
+    if not ONLY_SHOW:
+        os.system(command)
+
+def run(inputFolder, outputFolder, numberTiles):
     
     # Check input parameters
     if not os.path.isdir(inputFolder) and not os.path.isfile(inputFolder):
         raise Exception('Error: Input folder does not exist!')
-    
     if os.path.isfile(outputFolder):
         raise Exception('Error: There is a file with the same name as the output folder. Please, delete it!')
     elif os.path.isdir(outputFolder) and os.listdir(outputFolder):
         raise Exception('Error: Output folder exists and it is not empty. Please, delete the data in the output folder!')
-    
+    # Get the number of tiles per dimension (x and y)
     axisTiles = math.sqrt(numberTiles) 
     if not axisTiles.is_integer():
         raise Exception('Error: Number of tiles must be a square number!')
     axisTiles = int(axisTiles)
     
     # Create output folder
-    os.system('mkdir -p ' + outputFolder)
+    executeCommand('mkdir -p ' + outputFolder)
     
-    connection = psycopg2.connect(postgresops.getConnectString(dbName, dbUser, dbPass, dbHost, dbPort))
-    cursor = connection.cursor()
+    # Get the global extent and the as well as number of points and input files
+    (inputFiles, _, numPoints, minX, minY, _, maxX, maxY, _, scaleX, scaleY, _) = lasops.getPCFolderDetails(inputFolder)
+    numInputFiles = len(inputFiles)
+
+    print '%s contains %d files with %d points. The XY extent is %.2f, %.2f, %.2f, %.2f' % (inputFolder, numInputFiles, numPoints, minX, minY, maxX, maxY)
     
-#    (inputFiles, _, numberPoints, minX, minY, _, maxX, maxY, _, scaleX, scaleY, _) = lasops.getPCFolderDetails(inputFolder)
-    (inputFiles, numberPoints, minX, minY, maxX, maxY, scaleX, scaleY) = ([], 640000000000, 13427.64, 306746.26, 278487.77, 615431.24, 0.01, 0.01)
+    # Compute the size of a tile in X and in Y
+    tSizeX = (maxX - minX) / float(axisTiles)
+    tSizeY = (maxY - minY) / float(axisTiles)
     
-    print '%s contains %d files with %d points. The XY extent is %.2f, %.2f, %.2f, %.2f' % (inputFolder, len(inputFiles), numberPoints, minX, minY, maxX, maxY)
-    
-    rangeX = maxX - minX 
-    rangeY = maxY - minY
-    
-    tileSizeX = rangeX / float(axisTiles)
-    tileSizeY = rangeY / float(axisTiles)
-    
-    tilesTableName = 'TILES'
-    tileCounter = 0
-    
-    cursor.execute("""
-    CREATE TABLE """ + tilesTableName + """ (
-        id integer,
-        geom public.geometry(Geometry,""" + str(srid) + """)
-    )""")
-    
+    # Generate the tiles
+    # For each tile we generate:
+    #   - A Shapely BBox with the extent of the tile
+    #   - The empty output folder for the files that will go in that tile
+    tiles = []
     for xIndex in range(axisTiles):
         for yIndex in range(axisTiles):
-            minTileX = minX + (xIndex * tileSizeX)
-            maxTileX = minX + ((xIndex+1) * tileSizeX)
-            minTileY = minY + (yIndex * tileSizeY)
-            maxTileY = minY + ((yIndex+1) * tileSizeY)
+            tMinX = minX + (xIndex * tSizeX)
+            tMaxX = minX + ((xIndex+1) * tSizeX)
+            tMinY = minY + (yIndex * tSizeY)
+            tMaxY = minY + ((yIndex+1) * tSizeY)
             # To avoid overlapping tiles
             if xIndex < axisTiles-1:
-                maxTileX -= scaleX
+                tMaxX -= scaleX
             if yIndex < axisTiles-1:
-                maxTileY -= scaleY
-                
-            print '\t'.join((str(xIndex), str(yIndex), '%.2f' % minTileX, '%.2f' % minTileY, '%.2f' % maxTileX, '%.2f' % maxTileY))
-            insertStatement = "INSERT INTO " + tilesTableName + """(id,geom) VALUES (%s, ST_MakeEnvelope(%s, %s, %s, %s, %s));"""
-            insertArgs = [tileCounter, minTileX, minTileY, maxTileX, maxTileY, srid]
-            cursor.execute(insertStatement, insertArgs)
+                tMaxY -= scaleY
+            
+            tName = ('tile_%d_%d') % (int(tMinX / scaleX), int(tMinY / scaleY))
+            tiles.append(tName, tMinX, tMinY, tMaxX, tMaxY, box(tMinX, tMinY, tMaxX, tMaxY))
+            executeCommand('mkdir -p ' + outputFolder + '/' + tName)
     
-            tileCounter += 1
+    # Process the input files
+    # For each file we check which tiles it overlaps:
+    #   - If it is completely inside a tile we copy the file to the output folder of the tile
+    #   - If it is partially overlapping many tiles, we run lasmerge to cut the file into the pieces that need to go to each tile folder
+    for i in range(numInputFiles):
+        inputFile = inputFiles[i]
+        (_, _, minX, minY, _, maxX, maxY, _, _, _, _, _, _, _) = lasops.getPCFileDetails(inputFile)
+        geom = box(minX, minY, maxX, maxY)
+        for (tName, tMinX, tMinY, tMaxX, tMaxY, tBox) in tiles:
+            relation = _relation(tBox, geom)
+            if relation == 1:
+                command = 'cp ' + inputFile + ' ' + outputFolder + '/' + tName
+                break # If it is completely inside one tile, we do not need to check the rest
+            elif relation == 2:
+                command = 'lasmerge -i ' + inputFile + ' -inside ' + str(tMinX) + ' ' + str(tMinY) + ' ' + str(tMaxX) + ' ' + str(tMaxY) + ' -o ' + outputFolder + '/' + tName + '/cut_' + os.path.basename(inputFile) 
+            executeCommand(command)
+            print (i+1),'/',numInputFiles 
             
-    for inputFile in inputFiles:
-        (_, _, minX, minY, _, maxX, maxY, _, _, _, _, _, _, _) = lasops.getPCFileDetails(absPath)
-        # Check how many tiles it overlaps
-        # if one, we just copy it to the tile folder
-        # if many we run lasmerge top create the different parts!
+    # Check that the number of points after tiling is the same as initial
+    numPointsTiles = 0
+    numFilesTiles = 0
+    for (tName, _, _, _, _, _) in tiles:
+        (tInputFiles, _, tNumPoints, _, _, _, _, _, _, _, _, _) = lasops.getPCFolderDetails(outputFolder + '/' + tName)
+        numPointsTiles += tNumPoints
+        numFilesTiles += len(tInputFiles)
         
-            
+    if numPointsTiles != numPoints:
+        print 'Warning: #input_points = %d   #output_points = %d' % (numPoints, numPointsTiles)
+    else:
+        print '#input_points = #output_points = %d' % numPointsTiles
+    print '#input_files = %d   #output_files = %d' % (numInputFiles, numFilesTiles)
+    
 if __name__ == "__main__":
     args = argument_parser().parse_args()
     print 'Input folder: ', args.input
-    print 'SRID: ', args.srid  
     print 'Output folder: ', args.output
     print 'Number of tiles: ', args.number
-    print 'DB name: ', args.dbname
-    print 'DB user: ', args.dbuser
-    print 'DB pass: ', '*' * len(args.dbpass)
-    print 'DB host: ', args.dbhost
-    print 'DB port: ', args.dbport
     
     try:
         t0 = time.time()
         print 'Starting ' + os.path.basename(__file__) + '...'
-        run(args.input, args.srid, args.output, args.number, args.dbname, args.dbuser, args.dbpass, args.dbhost, args.dbport)
+        run(args.input, args.output, args.number)
         print 'Finished in %.2f seconds' % (time.time() - t0)
     except:
         print 'Execution failed!'
